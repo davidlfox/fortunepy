606faf473b8d1aabe1304eb46c9ad2e2dfaab229
David Fox <me@iamdavidfox.com>
2021-02-13 11:41:10 -0500

    generate the game

--- game.py
+++ game.py
@@ -1,8 +1,21 @@
 from fortune import Fortune
 from teller import Teller
 
-fortune = Fortune(1, "hello")
-print(fortune)
+# generate fortunes
+fortunes = {}
+for x in range(8):
+    num = x+1
+    if x % 2 == 0:
+        fortuneText = 'crappy fortune'
+    else:
+        fortuneText = 'happy fortune'
 
-teller = Teller(["blue", "red"], False, [])
+    fortunes[num] = Fortune(num, f'{fortuneText} {num}!')
+
+# generate teller
+teller = Teller(["blue", "red", "green", "yellow"], False, fortunes)
 print(teller)
+for x in teller.fortunes:
+    print(fortunes[x])
+
+# todo: the game
